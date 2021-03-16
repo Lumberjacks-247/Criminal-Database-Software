c6ed07e5bbcefd7ad8c4d8bbd93edf9d42e3ab71
Brooks Robinson <pyrogoggles@live.com>
2021-03-16 12:12:20 -0400

    implement people loading and writing, add consts

--- DataLoader.java
+++ DataLoader.java
@@ -6,6 +6,36 @@ import org.json.simple.parser.JSONParser;
 
 public class DataLoader extends DataConstants {
     public static ArrayList<Person> loadPeople() {
-        return new ArrayList<Person>();
+        ArrayList<Person> people = new ArrayList<Person>();
+
+        try {
+            FileReader reader = new FileReader(PERSON_FILE_NAME);
+            JSONParser parser = new JSONParser();
+            JSONArray peopleJSON = (JSONArray)new JSONParser().parse(reader);
+
+            for (int i = 0; i < peopleJSON.size(); ++i) {
+                JSONObject personJSON = (JSONObject)peopleJSON.get(i);
+                String id = (String)personJSON.get(PERSON_ID);
+                String name = (String)personJSON.get(PERSON_NAME);
+                String sex = (String)personJSON.get(PERSON_SEX);
+                String birthday = (String)personJSON.get(PERSON_BIRTHDAY);
+                int heightFt = ((Long)personJSON.get(PERSON_HEIGHT_FT)).intValue();
+                int heightIn = ((Long)personJSON.get(PERSON_HEIGHT_IN)).intValue();
+                int weight = ((Long)personJSON.get(PERSON_WEIGHT)).intValue();
+                boolean isJuvenile = (Boolean)personJSON.get(PERSON_IS_JUVENILE);
+                String address = (String)personJSON.get(PERSON_ADDRESS);
+                String city = (String)personJSON.get(PERSON_CITY);
+                String state = (String)personJSON.get(PERSON_STATE);
+                String zip  = (String)personJSON.get(PERSON_ZIP);
+                String eyeColor = (String)personJSON.get(PERSON_EYE_COLOR);
+                String race = (String)personJSON.get(PERSON_RACE);
+            }
+
+            return people;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
