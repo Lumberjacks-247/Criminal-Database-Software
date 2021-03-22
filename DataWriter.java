9afb4e9fbefc6b1b48d1991668f90ae32d35e005
Brooks Robinson <pyrogoggles@live.com>
2021-03-22 17:13:39 -0400

    added all saves for dwriter

--- DataWriter.java
+++ DataWriter.java
@@ -32,47 +32,191 @@ public class DataWriter extends DataConstants{
     }
 
 	public static void saveVictims() {
-		// TODO Auto-generated method stub
-		
+		Victims victims = Victims.getInstance();
+		ArrayList<Victim> victimList = victims.getVictims();
+		JSONArray jsonVictims = new JSONArray();
+
+		for (int i = 0; i < victimList.size(); ++i) {
+			jsonVictims.add(getVictimJSON(victimList.get(i)));
+		}
+
+		try (FileWriter file = new FileWriter(VICTIM_FILE_NAME)) {
+			file.write(jsonVictims.toJSONString());
+			file.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static JSONObject getVictimJSON(Victim victim) {
+		// TODO complete
 	}
 
 	public static void saveCrimes() {
-		// TODO Auto-generated method stub
-		
+		Crimes crimes = Crimes.getInstance();
+		ArrayList<Crime> crimeList = crimes.getCrimes();
+		JSONArray jsonCrimes = new JSONArray();
+
+		for (int i = 0; i < crimeList.size(); ++i) {
+			jsonCrimes.add(getCrimeJSON(crimeList.get(i)));
+		}
+
+		try (FileWriter file = new FileWriter(CRIMES_FILE_NAME)) {
+			file.write(jsonCrimes.toJSONString());
+			file.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static JSONObject getCrimeJSON(Crime crime) {
+		// TODO complete
 	}
 
 	public static void saveWitnesses() {
-		// TODO Auto-generated method stub
-		
+		Witnesses witnesses = Witnesses.getInstance();
+		ArrayList<Witness> witnessList = witnesses.getWitnesses();
+		JSONArray jsonWitnesses = new JSONArray();
+
+		for (int i = 0; i < witnessList.size(); ++i) {
+			jsonWitnesses.add(getWitnessJSON(witnessList.get(i)));
+		}
+
+		try (FileWriter file = new FileWriter(WITNESS_FILE_NAME)) {
+			file.write(jsonWitnesses.toJSONString());
+			file.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static JSONObject getWitnessJSON(Witness witness) {
+		// TODO complete
 	}
 
 	public static void saveCriminals() {
-		// TODO Auto-generated method stub
-		
+		Criminals criminals = Criminals.getInstance();
+		ArrayList<Criminal> criminalList = criminals.getCriminals();
+		JSONArray jsonCriminals = new JSONArray();
+
+		for (int i = 0; i < criminalList.size(); ++i) {
+			jsonCriminals.add(getCriminalJSON(criminalList.get(i)));
+		}
+
+		try (FileWriter file = new FileWriter(CRIMINAL_FILE_NAME)) {
+			file.write(jsonCriminals.toJSONString());
+			file.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static JSONObject getCriminalJSON(Criminal criminal) {
+		// TODO complete
 	}
 
 	public static void saveEvidences() {
-		// TODO Auto-generated method stub
-		
+		Evidences evidences = Evidences.getInstance();
+		ArrayList<Evidence> evidenceList = evidences.getEvidences();
+		JSONArray jsonEvidences = new JSONArray();
+
+		for (int i = 0; i < evidenceList.size(); ++i) {
+			jsonEvidences.add(getEvidenceJSON(evidenceList.get(i)));
+		}
+
+		try (FileWriter file = new FileWriter(EVIDENCE_FILE_NAME)) {
+			file.write(jsonEvidences.toJSONString());
+			file.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static JSONObject getEvidenceJSON(Evidence evidence) {
+		// TODO complete
 	}
 
 	public static void saveOfficers() {
-		// TODO Auto-generated method stub
-		
+		Officers officers = Officers.getInstance();
+		ArrayList<Officer> officerList = officers.getOfficers();
+		JSONArray jsonOfficers = new JSONArray();
+
+		for (int i = 0; i < officerList.size(); ++i) {
+			jsonOfficers.add(getOfficerJSON(officerList.get(i)));
+		}
+
+		try (FileWriter file = new FileWriter(OFFICER_FILE_NAME)) {
+			file.write(jsonOfficers.toJSONString());
+			file.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static JSONObject getOfficerJSON(Officer officer) {
+		// TODO complete
 	}
 
 	public static void savePeople() {
-		// TODO Auto-generated method stub
-		
+		People people = People.getInstance();
+		ArrayList<Person> personList = people.getPeople();
+		JSONArray jsonPeople = new JSONArray();
+
+		for (int i = 0; i < personList.size(); ++i) {
+			jsonPeople.add(getPersonJSON(personList.get(i)));
+		}
+
+		try (FileWriter file = new FileWriter(PERSON_FILE_NAME)) {
+			file.write(jsonPeople.toJSONString());
+			file.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static JSONObject getPersonJSON(Person person) {
+		// TODO complete
 	}
 
 	public static void savePOIs() {
-		// TODO Auto-generated method stub
-		
+		POIs pois = POIs.getInstance();
+		ArrayList<POI> poiList = pois.getPOIs();
+		JSONArray jsonPois = new JSONArray();
+
+		for (int i = 0; i < poiList.size(); ++i) {
+			jsonPois.add(getPOIJSON(poiList.get(i)));
+		}
+
+		try (FileWriter file = new FileWriter(POI_FILE_NAME)) {
+			file.write(jsonPois.toJSONString());
+			file.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static JSONObject getPOIJSON(POI poi) {
+		// TODO complete
 	}
 
 	public static void saveSuspects() {
-		// TODO Auto-generated method stub
-		
+		Suspects suspects = Suspects.getInstance();
+		ArrayList<Suspect> suspectList = suspects.getSuspects();
+		JSONArray jsonSuspects = new JSONArray();
+
+		for (int i = 0; i < suspectList.size(); ++i) {
+			jsonSuspects.add(getSuspectJSON(suspectList.get(i)));
+		}
+
+		try (FileWriter file = new FileWriter(SUSPECTS_FILE_NAME)) {
+			file.write(jsonSuspects.toJSONString());
+			file.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static JSONObject getSuspectJSON(Suspect suspect) {
+		// TODO complete
 	}
 }
